054f8cbd832e050a8a08724ebd798008328d46e5
Kiran Eiden <kiraneiden@gmail.com>
2022-04-16 01:03:57 -0700

    Add bisection search TOV limit calculation

--- tov_integrator.py
+++ tov_integrator.py
@@ -25,6 +25,10 @@ outdir_help = """The path to the output directory. If this argument is not suppl
         will be located in the same directory as the eos_table."""
 profiles_help = """If supplied, will output a profile of pressure, mass, and energy density as a
         function of radius for each choice of central pressure."""
+nmtov_help = """If supplied will not calculate the TOV limit, which requires refining the search
+        near peak mass."""
+mtov_prec_help = """Precision to determine the central pressure associated with the TOV limit to, as
+        a fraction of the central pressure estimate."""
 
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('eos_table', help=eos_table_help)
@@ -35,6 +39,8 @@ parser.add_argument('-l', '--linear', action='store_true', help=linear_help)
 parser.add_argument('-f', '--outfile', default="mr_rel.txt", help=outfile_help)
 parser.add_argument('-d', '--outdir', help=outdir_help)
 parser.add_argument('-p', '--profiles', action='store_true', help=profiles_help)
+parser.add_argument('--noMtov', action='store_true', help=nmtov_help)
+parser.add_argument('--Mtov_prec', type=int, default=1e-6, help=mtov_prec_help)
 
 args = parser.parse_args(sys.argv[1:])
 
@@ -42,6 +48,7 @@ args = parser.parse_args(sys.argv[1:])
 G  = 6.67430e-8 # cm**3 / g / s**2
 kB = 1.380649e-16 # erg/K
 c  = 2.99792458e10 # cm/s
+φ  = (1 + np.sqrt(5)) / 2
 
 # Unit conversions
 MeV_erg = 1.6021766339999998e-06
@@ -111,6 +118,56 @@ def integrate_TOV(step, P0):
     system = System(Pprime, Mprime, init_conds=(r0, P0, M0))
     res = np.array(list(rk4(system, step, completed)))
     return res
+    
+def calculate_Mtov(step, P_c, M):
+    """Determine the TOV limit by refining the search near the peak mass."""
+
+    print("\nCalculating TOV limit...")
+
+    new_P_c = []
+    new_results = []
+    
+    i_peak = np.argmax(M)
+    M_tov = M[i_peak]
+    
+    if (i_peak == 0) or (i_peak == len(M)-1):
+        print("TOV limit cannot be determined with this central pressure range.")
+        return new_P_c, new_results
+        
+    thresh = args.Mtov_prec
+    P0, P1, P2 = P_c[i_peak-1:i_peak+2]
+    
+    while (P2 - P0)/P1 > thresh:
+        
+        P_1_2 = 0.5 * (P0 + P1)
+        new_res = integrate_TOV(step, P_1_2)
+        new_M = new_res[-1][2]
+        
+        if new_M > M_tov:
+            
+            M_tov = new_M
+            P2 = P1; P1 = P_1_2
+            new_P_c.append(P_1_2)
+            new_results.append(new_res)
+            continue
+            
+        P_3_2 = 0.5 * (P1 + P2)
+        new_res = integrate_TOV(step, P_3_2)
+        new_M = new_res[-1][2]
+        
+        if new_M > M_tov:
+            
+            M_tov = new_M
+            P0 = P1; P1 = P_3_2
+            new_P_c.append(P_3_2)
+            new_results.append(new_res)
+            continue
+            
+        P0 = P_1_2
+        P2 = P_3_2
+    
+    print(f"TOV limit: {M_tov*g_Msun:.3f}")
+    return new_P_c, new_results
 
 def progress_bar(frac, size=50):
     
@@ -122,14 +179,29 @@ def progress_bar(frac, size=50):
         end = '\n'
     print(bar, f'{round(100*frac)}%', end=end)
 
-# Do integration for each central pressure    
+# Do integration for each central pressure
+print("Calculating M(R)...")
+
 dr = 1e2
 results = [None]*len(P_c)
 progress_bar(0)
 for i, P_ci in enumerate(P_c):
     results[i] = integrate_TOV(dr, P_ci)
     progress_bar((i+1)/len(P_c))
+
+# Calculate TOV limit
+if not args.noMtov:
     
+    M = np.array([res[-1][2] for res in results])
+    new_P_c, new_results = calculate_Mtov(dr, P_c, M)
+    P_c = np.append(P_c, new_P_c)
+    results += new_results
+    idx = np.argsort(P_c)
+    
+    P_c = P_c[idx]
+    results = [results[i] for i in idx]
+    
+# Get radii and masses for mass-radius relation
 R = np.array([res[-1][0] for res in results])
 M = np.array([res[-1][2] for res in results])
 
